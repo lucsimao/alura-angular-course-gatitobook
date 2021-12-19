75aabcb2b13015bcfd0e96f7782b39253e5ec4f3
lucsimao <lucasimao64@gmail.com>
2021-12-18 23:09:21 -0300

    feat: add login to home page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   imports: [CommonModule, HomeRoutingModule],
   exports: [HomeComponent],
 })
