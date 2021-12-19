190f8ba922c38596aa644d141f8062407aca7cd0
lucsimao <lucasimao64@gmail.com>
2021-12-18 22:38:51 -0300

    feat: create home initial layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { HomeRoutingModule } from './home-routing.module';
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
