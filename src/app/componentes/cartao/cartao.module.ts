30058dac281cb29684c04b18febc115dde5acfce
lucsimao <lucasimao64@gmail.com>
2021-12-20 14:34:24 -0300

    feat: create cartao component

---
+++ src/app/componentes/cartao/cartao.module.ts
@@ -0,0 +1,10 @@
+import { CartaoComponent } from './cartao.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [CartaoComponent],
+  imports: [CommonModule],
+  exports: [CartaoComponent],
+})
+export class CartaoModule {}
