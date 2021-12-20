4595dea9d7dd07ca730f31f4d5161c6195a46b5b
lucsimao <lucasimao64@gmail.com>
2021-12-20 01:29:29 -0300

    feat: add usuarioService to autenticacaoService

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
 
 const url = 'http://localhost:3000/user/login';
 
@@ -8,9 +10,26 @@ const url = 'http://localhost:3000/user/login';
   providedIn: 'root',
 })
 export class AutenticacaoService {
-  constructor(private readonly httpClient: HttpClient) {}
+  constructor(
+    private readonly httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  public autentica(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post(url, { userName: usuario, password: senha });
+  public autentica(
+    usuario: string,
+    senha: string
+  ): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        url,
+        { userName: usuario, password: senha },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
