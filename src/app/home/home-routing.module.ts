984444318abd5baad41b2cbed37426ba25aee84c
lucsimao <lucasimao64@gmail.com>
2021-12-19 21:00:38 -0300

    feat: create novo-usuario page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    children: [{ path: '', component: LoginComponent }],
+    children: [
+      { path: '', component: LoginComponent },
+      { path: 'novo-usuario', component: NovoUsuarioComponent },
+    ],
   },
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { LoginComponent } from './login/login.component';
 import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { NgModule } from '@angular/core';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
   imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
   exports: [HomeComponent],
 })
