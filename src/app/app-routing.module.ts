4febb2e9714026d16aac335242a0688cfaef84bf
lucsimao <lucasimao64@gmail.com>
2021-12-20 21:10:16 -0300

    feat: add guard to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
@@ -8,6 +9,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard],
   },
   {
     path: 'animais',
