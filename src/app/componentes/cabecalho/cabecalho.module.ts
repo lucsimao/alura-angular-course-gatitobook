b52c27ac5a3cb38555e208dcd60bdf399cf24ec5
lucsimao <lucasimao64@gmail.com>
2021-12-21 19:57:24 -0300

    feat: add menu to cabecalho

--- src/app/componentes/cabecalho/cabecalho.module.ts
+++ src/app/componentes/cabecalho/cabecalho.module.ts
@@ -1,11 +1,12 @@
 import { CabecalhoComponent } from './cabecalho.component';
 import { CommonModule } from '@angular/common';
+import { MenuModule } from './../menu/menu.module';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CabecalhoComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuModule],
   exports: [CabecalhoComponent],
 })
 export class CabecalhoModule {}
