1d210dfa6ad2331abaafc9b5c2cd4bd2f127cc63
lucsimao <lucasimao64@gmail.com>
2021-12-21 19:48:26 -0300

    feat: create menu component

---
+++ src/app/componentes/menu/menu.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [MenuComponent],
+  imports: [CommonModule],
+  exports: [MenuComponent],
+})
+export class MenuModule {}
